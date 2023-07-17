822f66f8fbbfaf62f9b1b03e4b4c62739b52d7e3
Ihor <dimov.igor97@gmail.com>
2023-07-17 12:20:25 +0300

    todolists tsx

---
+++ src/Todolist.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+type todolistType = {
+    title:string
+}
+
+export const Todolist:React.FC<todolistType> = ({title}) => {
+  return (
+    <div>
+                <h3>{title}</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+  )
+}
